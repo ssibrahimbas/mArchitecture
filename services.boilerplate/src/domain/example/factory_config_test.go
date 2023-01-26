71ead05f16d9bf97f08a2406935ab702d8cdb06b
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2023-01-27 02:05:39 +0300

    ci: add test pipeline and add example test

---
+++ services.boilerplate/src/domain/example/factory_config_test.go
@@ -0,0 +1,41 @@
+package example
+
+import "testing"
+
+func TestFactoryConfig_Validate(t *testing.T) {
+	type fields struct {
+		MinFieldLength   int
+		MaxFieldLength   int
+		MinContentLength int
+		MaxContentLength int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "valid config",
+			fields: fields{
+				MinFieldLength:   1,
+				MaxFieldLength:   100,
+				MinContentLength: 1,
+				MaxContentLength: 100,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := FactoryConfig{
+				MinFieldLength:   tt.fields.MinFieldLength,
+				MaxFieldLength:   tt.fields.MaxFieldLength,
+				MinContentLength: tt.fields.MinContentLength,
+				MaxContentLength: tt.fields.MaxContentLength,
+			}
+			if err := f.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("FactoryConfig.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
